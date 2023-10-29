f1b3b1853fee36bb27236ffbce3e544d8b18616c
Matt R <mattrallo2012@gmail.com>
2023-10-29 11:45:53 -0500

    able to store input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,16 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
 var today = dayjs(); // get todays information using dayjs
 
 var calendarContainer = $("#calendarContainer"); // get container for input fields
 
+
 displayCurrentDate(); // call the function to display the current day on webpage
 displayInputFields(); // diplays all the input fields on the webpage
+
+//create a click event for each button
+for(var i = 9; i < 18; i++){
+  var button = $("#button-hour-" + i);
+  button.on('click', buttonClickEvent);
+}
 
 function displayCurrentDate(){ // displays date on webpage
   var weekDay = dayjs(today, "M-D-YYYY").format("dddd"); // get the day of the week
@@ -61,6 +44,7 @@ function displayInputFields(){
     var inputField = $("<textarea>");
     inputField.addClass("col-8 col-md-10 description"); // add classes
     inputField.attr("rows", "3"); // set rows for textarea
+    inputField.attr("id", "input-hour-" + hour); // set id for each input (to use later)
     // check hour to set background color class
     // grey for past, red for present, green for future
     // classes are set in style.css
@@ -79,6 +63,7 @@ function displayInputFields(){
     var button = $("<button>");
     button.addClass("btn saveBtn col-2 col-md-1");
     button.attr("aria-label", "save");
+    button.attr("id", "button-hour-" + hour); // set id for each button (to use later)
     // create i element for button
     var i = $("<i>");
     i.addClass("fas fa-save");
@@ -90,4 +75,18 @@ function displayInputFields(){
 
     hour ++; // go to next hour
   }
+}
+
+function buttonClickEvent(){
+
+  var hour = $(this).attr("id").match(/\d+/g); // get string id of button and find hour integer within said string 
+  // d refers to digits (regexp) and g finds all matches
+  // .match then sees if it matches
+
+  // get input
+  var task = $(this).parent().children('textarea').val(); 
+  
+  // save input into local storage
+  localStorage.setItem(hour, task); //use hour for key
+
 }
